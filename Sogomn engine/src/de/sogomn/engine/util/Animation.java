9c0264770cd1635c40196e1913caa0432600d0c0
Sogomn <johannes.boczek@web.de>
2015-07-26 01:16:32 +0200

    Animations can now have a max amount of loops

--- Sogomn engine/src/de/sogomn/engine/util/Animation.java
+++ Sogomn engine/src/de/sogomn/engine/util/Animation.java
@@ -18,6 +18,10 @@ public final class Animation implements ITickable {
 	
 	private float delay, timer;
 	
+	private int maxLoops, currentLoop;
+	
+	public static final int INFINITE = -1;
+	
 	/**
 	 * Constructs an Animation object with the given delay between the images and the passed images.
 	 * @param delay The delay between the images in seconds
@@ -26,21 +30,47 @@ public final class Animation implements ITickable {
 	public Animation(final float delay, final BufferedImage... images) {
 		this.delay = delay;
 		this.images = images;
+		
+		maxLoops = INFINITE;
 	}
 	
 	@Override
 	public void update(final float delta) {
+		if (maxLoops != INFINITE && currentLoop >= maxLoops) {
+			return;
+		}
+		
 		timer += delta;
 		
 		if (timer >= delay) {
 			currentIndex++;
-			currentIndex %= (images.length - 1);
+			
+			if (currentIndex > images.length - 1) {
+				currentIndex = 0;
+				currentLoop++;
+			}
 		}
 	}
 	
 	@Override
 	public void draw(final Graphics2D g) {
 		//...
+	}
+	
+	/**
+	 * Resets the timer, the index and the loop count.
+	 */
+	public void reset() {
+		timer = 0;
+		currentIndex = currentLoop = 0;
+	}
+	
+	/**
+	 * Sets the maximum amount on loops the animation will go through. Default is INFINITE (-1).
+	 * @param maxLoops The maximum amount of loops
+	 */
+	public void setMaxLoops(final int maxLoops) {
+		this.maxLoops = maxLoops;
 	}
 	
 	/**
@@ -65,6 +95,14 @@ public final class Animation implements ITickable {
 	 */
 	public BufferedImage getImage() {
 		return images[currentIndex];
+	}
+	
+	/**
+	 * Returns the current loop the animation is in.
+	 * @return The loop count
+	 */
+	public int getCurrentLoop() {
+		return currentLoop;
 	}
 	
 }
