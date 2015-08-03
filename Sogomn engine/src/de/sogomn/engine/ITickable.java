a37ecf8162352e8749ae94d46ca91cb05583fb60
Sogomn <johannes.boczek@web.de>
2015-08-03 18:18:30 +0200

    Removed JavaDoc Changed some comments

--- Sogomn engine/src/de/sogomn/engine/ITickable.java
+++ Sogomn engine/src/de/sogomn/engine/ITickable.java
@@ -4,6 +4,7 @@ package de.sogomn.engine;
 /**
  * Defines an interface to be used for the Screen class and any object that "ticks".
  * Inherits from IUpdatable and IDrawable.
+ * Can be used in other context aswell.
  * @author Sogomn
  *
  */
