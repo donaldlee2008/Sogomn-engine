27b0fd7c3b0c7af27d5e89c143fd67971e693aa8
Sogomn <Sogomn@Sogomn-PC>
2016-01-22 17:04:42 +0100

    Major changes The scheduler does no longer handle the tasks one after another

--- Sogomn engine/src/de/sogomn/engine/util/Scheduler.java
+++ Sogomn engine/src/de/sogomn/engine/util/Scheduler.java
@@ -1,22 +1,20 @@
 package de.sogomn.engine.util;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import de.sogomn.engine.IUpdatable;
 
 /**
  * The Scheduler class can be used to schedule tasks.
  * The method "update" needs to be called regularly in order to work.
- * It will execute the tasks one after another.
+ * Does not use a separate thread. All methods are synchronized with the internal list.
  * @author Sogomn
  *
  */
 public final class Scheduler implements IUpdatable {
 	
-	private double timer;
-	
 	private ArrayList<Task> tasks;
-	private Task currentTask;
 	
 	/**
 	 * Constructs a new Scheduler which can execute tasks.
@@ -25,24 +23,25 @@ public final class Scheduler implements IUpdatable {
 		tasks = new ArrayList<Task>();
 	}
 	
-	private boolean isDone(final Task task) {
-		if (timer >= task.time) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
 	/**
 	 * Updates the scheduler.
 	 */
 	@Override
 	public void update(final double delta) {
-		timer += delta;
-		
-		if (currentTask != null && isDone(currentTask)) {
-			currentTask.runnable.run();
-			removeTask(currentTask);
+		synchronized (tasks) {
+			final Iterator<Task> iterator = tasks.iterator();
+			
+			while (iterator.hasNext()) {
+				final Task task = iterator.next();
+				
+				if (task.isDone()) {
+					iterator.remove();
+					
+					task.execute();
+				}
+				
+				task.update(delta);
+			}
 		}
 	}
 	
@@ -50,8 +49,9 @@ public final class Scheduler implements IUpdatable {
 	 * Removes all tasks from the schedule.
 	 */
 	public void clearTasks() {
-		currentTask = null;
-		tasks.clear();
+		synchronized (tasks) {
+			tasks.clear();
+		}
 	}
 	
 	/**
@@ -59,11 +59,8 @@ public final class Scheduler implements IUpdatable {
 	 * @param task The task
 	 */
 	public void addTask(final Task task) {
-		tasks.add(task);
-		
-		if (currentTask == null) {
-			timer = 0;
-			currentTask = task;
+		synchronized (tasks) {
+			tasks.add(task);
 		}
 	}
 	
@@ -72,34 +69,9 @@ public final class Scheduler implements IUpdatable {
 	 * @param task The task to be removed
 	 */
 	public void removeTask(final Task task) {
-		tasks.remove(task);
-		
-		if (currentTask == task) {
-			timer = 0;
-			currentTask = getNextTask();
+		synchronized (tasks) {
+			tasks.remove(task);
 		}
-	}
-	
-	/**
-	 * Returns the internal timer in seconds.
-	 * @return The timer
-	 */
-	public double getTimer() {
-		return timer;
-	}
-	
-	/**
-	 * Returns the next task to be executed.
-	 * @return The task
-	 */
-	public Task getNextTask() {
-		if (!hasTask()) {
-			return null;
-		}
-		
-		final Task task = tasks.get(0);
-		
-		return task;
 	}
 	
 	/**
@@ -107,22 +79,25 @@ public final class Scheduler implements IUpdatable {
 	 * @return True if there is a task scheduled; false otherwise.
 	 */
 	public boolean hasTask() {
-		return !tasks.isEmpty();
+		synchronized (tasks) {
+			return !tasks.isEmpty();
+		}
 	}
 	
 	/**
-	 * Can be scheduled with the help of the Scheduler class.
+	 * Can be scheduled with the help of the Scheduler class
 	 * @author Sogomn
 	 *
 	 */
-	public static class Task {
+	public static final class Task {
 		
 		private Runnable runnable;
+		private double timer;
 		private float time;
 		
 		/**
 		 * Constructs a new Task object.
-		 * @param runnable The method "run" will be called when the task gets executed
+		 * @param executable The method "execute" will be called when the task gets executed
 		 * @param time The time the task should be executed after
 		 */
 		public Task(final Runnable runnable, final float time) {
@@ -131,11 +106,33 @@ public final class Scheduler implements IUpdatable {
 		}
 		
 		/**
+		 * Updates the task. Automatically called by the Scheduler class.
+		 */
+		public void update(final double delta) {
+			timer += delta;
+		}
+		
+		/**
 		 * Returns the time the task should be executed after.
 		 * @return The time
 		 */
-		public final float getTime() {
+		public float getTime() {
 			return time;
+		}
+		
+		/**
+		 * Returns whether this Task is done or not.
+		 * @return The state
+		 */
+		public boolean isDone() {
+			return timer >= time;
+		}
+		
+		/**
+		 * Executes this task. Ignores the timer.
+		 */
+		public void execute() {
+			runnable.run();
 		}
 		
 	}
